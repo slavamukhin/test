bbc3f41f49146f0ebc6c340addab13cc0b1efd09
slavamukhin <via4eslav.mukhin@yandex.ru>
2022-03-14 10:13:16 +0300

    bug is fixed

--- src/keycloak.ts
+++ src/keycloak.ts
@@ -1,10 +1,12 @@
 import Keycloak from 'keycloak-js'
 
-const SEPARATOR = '.'
+const SEPARATOR = '-'
 const hostname = window.location.hostname
 const arrayHostName: Array<string> = hostname.split(SEPARATOR)
 arrayHostName.shift()
 const partNameHost = arrayHostName.join(SEPARATOR)
+
+console.log('partNameHost', partNameHost)
 
 const keycloak = Keycloak({
   url: `https://keycloak-${partNameHost}/auth`,
